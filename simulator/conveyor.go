01b66a5478b2cb5962c6eadf8f9545585305d4a8
go-aioesphomeapi contributors <flavio-fernandes@users.noreply.github.com>
2026-07-17 07:33:27 -0400

    simulator: add MGMT loopback acceptance

--- simulator/conveyor.go
+++ simulator/conveyor.go
@@ -10,14 +10,15 @@ const (
 	StatusLightKey  uint32 = 102
 	EntrySensorKey  uint32 = 103
 	ExitSensorKey   uint32 = 104
-	RedSensorKey    uint32 = 105
-	GreenSensorKey  uint32 = 106
-	BlueSensorKey   uint32 = 107
-	ClearSensorKey  uint32 = 108
-	EnableSwitchKey uint32 = 109
-	SpeedNumberKey  uint32 = 110
-	ResetButtonKey  uint32 = 111
-	StatusTextKey   uint32 = 112
+	RunRequestKey   uint32 = 105
+	RedSensorKey    uint32 = 106
+	GreenSensorKey  uint32 = 107
+	BlueSensorKey   uint32 = 108
+	ClearSensorKey  uint32 = 109
+	EnableSwitchKey uint32 = 110
+	SpeedNumberKey  uint32 = 111
+	ResetButtonKey  uint32 = 112
+	StatusTextKey   uint32 = 113
 )
 
 // ConveyorScenario models the entities required by the MGMT conveyor demo.
@@ -25,11 +26,12 @@ func ConveyorScenario() Scenario {
 	return Scenario{Name: "conveyor-simulator", Entities: []proto.Message{
 		&pb.ListEntitiesFanResponse{Key: ConveyorFanKey, ObjectId: "conveyor_motor", Name: "Conveyor Motor", SupportsSpeed: true, SupportsDirection: true, SupportedSpeedCount: 100},
 		&pb.ListEntitiesLightResponse{Key: StatusLightKey, ObjectId: "status_light", Name: "Status Light", SupportedColorModes: []pb.ColorMode{pb.ColorMode_COLOR_MODE_RGB}},
-		&pb.ListEntitiesBinarySensorResponse{Key: EntrySensorKey, ObjectId: "entry_present", Name: "Entry Present"},
-		&pb.ListEntitiesBinarySensorResponse{Key: ExitSensorKey, ObjectId: "exit_present", Name: "Exit Present"},
-		&pb.ListEntitiesSensorResponse{Key: RedSensorKey, ObjectId: "color_red", Name: "Color Red"},
-		&pb.ListEntitiesSensorResponse{Key: GreenSensorKey, ObjectId: "color_green", Name: "Color Green"},
-		&pb.ListEntitiesSensorResponse{Key: BlueSensorKey, ObjectId: "color_blue", Name: "Color Blue"},
+		&pb.ListEntitiesBinarySensorResponse{Key: EntrySensorKey, ObjectId: "entry_object_present", Name: "Entry Object Present"},
+		&pb.ListEntitiesBinarySensorResponse{Key: ExitSensorKey, ObjectId: "exit_object_present", Name: "Exit Object Present"},
+		&pb.ListEntitiesBinarySensorResponse{Key: RunRequestKey, ObjectId: "conveyor_run_request", Name: "Conveyor Run Request"},
+		&pb.ListEntitiesSensorResponse{Key: RedSensorKey, ObjectId: "entry_red", Name: "Entry Red"},
+		&pb.ListEntitiesSensorResponse{Key: GreenSensorKey, ObjectId: "entry_green", Name: "Entry Green"},
+		&pb.ListEntitiesSensorResponse{Key: BlueSensorKey, ObjectId: "entry_blue", Name: "Entry Blue"},
 		&pb.ListEntitiesSensorResponse{Key: ClearSensorKey, ObjectId: "color_clear", Name: "Color Clear"},
 		&pb.ListEntitiesSwitchResponse{Key: EnableSwitchKey, ObjectId: "conveyor_enable", Name: "Conveyor Enable"},
 		&pb.ListEntitiesNumberResponse{Key: SpeedNumberKey, ObjectId: "conveyor_speed", Name: "Conveyor Speed", MinValue: 0, MaxValue: 100, Step: 1},
@@ -38,7 +40,7 @@ func ConveyorScenario() Scenario {
 	}, States: []proto.Message{
 		&pb.FanStateResponse{Key: ConveyorFanKey, Direction: pb.FanDirection_FAN_DIRECTION_FORWARD},
 		&pb.LightStateResponse{Key: StatusLightKey, ColorMode: pb.ColorMode_COLOR_MODE_RGB},
-		&pb.BinarySensorStateResponse{Key: EntrySensorKey}, &pb.BinarySensorStateResponse{Key: ExitSensorKey},
+		&pb.BinarySensorStateResponse{Key: EntrySensorKey}, &pb.BinarySensorStateResponse{Key: ExitSensorKey}, &pb.BinarySensorStateResponse{Key: RunRequestKey},
 		&pb.SensorStateResponse{Key: RedSensorKey}, &pb.SensorStateResponse{Key: GreenSensorKey}, &pb.SensorStateResponse{Key: BlueSensorKey}, &pb.SensorStateResponse{Key: ClearSensorKey},
 		&pb.SwitchStateResponse{Key: EnableSwitchKey}, &pb.NumberStateResponse{Key: SpeedNumberKey},
 		&pb.TextSensorStateResponse{Key: StatusTextKey, State: "ready"},
